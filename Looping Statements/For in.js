4daeb4c07b90577c9b5d9a438030a3101341b6fb
iamnithintn <tnnithin1@gmail.com>
2023-03-23 16:02:22 +0530

    For Loops done, yet to learn For Each loop

---
+++ Looping Statements/For in.js
@@ -0,0 +1,25 @@
+//this is used to print or select the keys of an object
+
+
+let marks = {
+    harry:1,
+    me:2,
+    jo:3,
+    you:4
+}
+
+
+
+//printing the keys
+for (let a in marks) {
+    console.log(a);
+}
+
+
+
+//printing the values
+for (let k in marks) {
+    console.log(marks[k]);
+}
+
+
---
+++ Looping Statements/For of.js
@@ -0,0 +1,22 @@
+//this is used to print or select the iterables such as array's or String etc
+
+
+let value = "Godzilla"
+
+
+for (let a of value) {
+    console.log(a);
+}
+
+
+// let marks = {
+//     harry:1,
+//     me:2,
+//     jo:3,
+//     you:4
+// }
+
+// for (let a of marks) {                    -----> this gives an error because it is an object and not a iterable
+//     console.log(a);
+// }
+
